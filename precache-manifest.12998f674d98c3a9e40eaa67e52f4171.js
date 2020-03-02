bdcef3ccef14f77c66fec129c9365eec87946fe8
Bojan Krstic <krstic.bojan80@gmail.com>
2020-03-02 23:40:05 +0100

    Updates

---
+++ precache-manifest.12998f674d98c3a9e40eaa67e52f4171.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e3efe1601e124bcf9b",
+    "url": "/bojankrstic/static/css/main.434818b2.chunk.css"
+  },
+  {
+    "revision": "f3e3efe1601e124bcf9b",
+    "url": "/bojankrstic/static/js/main.f3e3efe1.chunk.js"
+  },
+  {
+    "revision": "8c8a272fada27f01f7dc",
+    "url": "/bojankrstic/static/js/runtime~main.8c8a272f.js"
+  },
+  {
+    "revision": "34f59e4a6bf740179b92",
+    "url": "/bojankrstic/static/js/2.34f59e4a.chunk.js"
+  },
+  {
+    "revision": "b5b5f104b83c949dc75172dc8f75499c",
+    "url": "/bojankrstic/static/media/my-picture.b5b5f104.JPG"
+  },
+  {
+    "revision": "d807476388a5816a74a332ca7bdcbcb1",
+    "url": "/bojankrstic/static/media/address.d8074763.png"
+  },
+  {
+    "revision": "bef46a7050ab237eff5d800eb51f4d92",
+    "url": "/bojankrstic/static/media/phone.bef46a70.jpg"
+  },
+  {
+    "revision": "6d431c3dd560c0fb8f1f55cadda3a1dc",
+    "url": "/bojankrstic/static/media/mail.6d431c3d.png"
+  },
+  {
+    "revision": "ea91a52834df68ac5e243e173bae0952",
+    "url": "/bojankrstic/static/media/bubble.ea91a528.jpg"
+  },
+  {
+    "revision": "44eb3d798c839c32f9dbb3a9d7f03937",
+    "url": "/bojankrstic/index.html"
+  }
+];
