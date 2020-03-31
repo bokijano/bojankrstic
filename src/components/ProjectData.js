8e5f4b569422b026cb465b7ff3f33ab23c5167b1
Bojan Krstic <krstic.bojan80@gmail.com>
2020-03-31 13:31:57 +0200

    new project commit

---
+++ src/components/ProjectData.js
@@ -0,0 +1,54 @@
+const projectList = [
+  {
+    id: 1,
+    title: "WORLD CUP RUSSIA 2018",
+    linkSrc:
+      "https://f2j3m8mdozkgkfs9mpn8vw-on.drv.tw/worldcup2018/sajt/worldcup.html",
+    atlText: "world cup"
+  },
+  {
+    id: 2,
+    title: "ALL ABOUT CARS",
+    linkSrc:
+      "https://f2j3m8mdozkgkfs9mpn8vw-on.drv.tw/cars/svetAutomobila/dist/automobilizam.html",
+    atlText: "cars"
+  },
+  {
+    id: 3,
+    title: "APPLICATION MANAGER",
+    linkSrc: "https://bokijano.github.io/app-manager",
+    atlText: "app manager"
+  },
+  {
+    id: 4,
+    title: "ONLINE SHOPPING APP",
+    linkSrc: "https://bokijano.github.io/online-commerce",
+    atlText: "shoping app"
+  },
+  {
+    id: 5,
+    title: "WEATHER FORECAST SERBIA",
+    linkSrc: "https://bokijano.github.io/forecast-serbia",
+    atlText: "weather forecast"
+  },
+  {
+    id: 6,
+    title: "MOUNTAIN RESORTS",
+    linkSrc: "https://bokijano.github.io/resorts",
+    atlText: "mountain resorts"
+  },
+  {
+    id: 7,
+    title: "YAMB GAME",
+    linkSrc: "https://bokijano.github.io/game-yamb",
+    atlText: "yamb game"
+  },
+  {
+    id: 8,
+    title: "MOVIES APP",
+    linkSrc: "https://bokijano.github.io/movie-app",
+    atlText: "movies appSrc"
+  }
+];
+
+export default projectList;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { FaSearch } from "react-icons/fa";
 
+import projects from "./ProjectData";
+
+import MoviesApp from "./../pictures/projects/movie-app.jpg";
 import Yamb from "./../pictures/projects/yamb.jpg";
 import Resorts from "./../pictures/projects/resorts.jpg";
 import Forecast from "./../pictures/projects/forecast-serbia.jpg";
@@ -10,125 +13,43 @@ import Cars from "./../pictures/projects/automobilizam.jpg";
 import Worldcup from "./../pictures/projects/worldcup.jpg";
 
 export default class Projects extends Component {
+  state = {
+    projectImg: [
+      Worldcup,
+      Cars,
+      ApplicationManager,
+      OnlineCommerce,
+      Forecast,
+      Resorts,
+      Yamb,
+      MoviesApp
+    ],
+    projects: projects
+  };
   render() {
     return (
       <div className="projects">
         <h1>Projects</h1>
         <article className="project-link">
-          <section className="project-image">
-            <img src={Yamb} alt="yamb" />
-            <div className="project-name">
-              <a href="https://bokijano.github.io/game-yamb" target="_blank">
-                <span>
-                  <FaSearch />
-                </span>
-                <br />
-                YAMB GAME
-              </a>
-            </div>
-          </section>
-
-          <section className="project-image">
-            <img src={Resorts} alt="resorts" />
-            <div className="project-name">
-              <a
-                href="https://bokijano.github.io/resorts"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <FaSearch />
-                </span>
-                <br />
-                MOUNTAIN RESORTS
-              </a>
-            </div>
-          </section>
-
-          <section className="project-image">
-            <img src={Forecast} alt="forecast" />
-            <div className="project-name">
-              <a
-                href="https://bokijano.github.io/forecast-serbia"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <FaSearch />
-                </span>
-                <br />
-                WEATHER FORECAST SERBIA
-              </a>
-            </div>
-          </section>
-
-          <section className="project-image">
-            <img src={OnlineCommerce} alt="online commerce" />
-            <div className="project-name">
-              <a
-                href="https://bokijano.github.io/online-commerce"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <FaSearch />
-                </span>
-                <br />
-                ONLINE SHOPPING APP
-              </a>
-            </div>
-          </section>
-
-          <section className="project-image">
-            <img src={ApplicationManager} alt="application manager" />
-            <div className="project-name">
-              <a
-                href="https://bokijano.github.io/app-manager"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <FaSearch />
-                </span>
-                <br />
-                APPLICATION MANAGER
-              </a>
-            </div>
-          </section>
-
-          <section className="project-image">
-            <img src={Cars} alt="cars" />
-            <div className="project-name">
-              <a
-                href="https://f2j3m8mdozkgkfs9mpn8vw-on.drv.tw/cars/svetAutomobila/dist/automobilizam.html"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <FaSearch />
-                </span>
-                <br />
-                ALL ABOUT CARS
-              </a>
-            </div>
-          </section>
-
-          <section className="project-image">
-            <img src={Worldcup} alt="world - cup" />
-            <div className="project-name">
-              <a
-                href="https://f2j3m8mdozkgkfs9mpn8vw-on.drv.tw/worldcup2018/sajt/worldcup.html"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span>
-                  <FaSearch />
-                </span>
-                <br />
-                WORLD CUP RUSSIA 2018
-              </a>
-            </div>
-          </section>
+          {this.state.projects.reverse().map(project => {
+            return (
+              <section key={project.id} className="project-image">
+                <img
+                  src={this.state.projectImg[project.id - 1]}
+                  alt={project.altText}
+                />
+                <div className="project-name">
+                  <a href={project.linkSrc} target="_blank">
+                    <span>
+                      <FaSearch />
+                    </span>
+                    <br />
+                    {project.title}
+                  </a>
+                </div>
+              </section>
+            );
+          })}
         </article>
       </div>
     );
